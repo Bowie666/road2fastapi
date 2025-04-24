d8bd472412a1fe6ff02c67dfdae9f3b0a05ad783
Bowie666 <ldldxw@163.com>
2025-04-24 09:47:21 +0800

    添加默认文档

--- app/main.py
+++ app/main.py
@@ -25,6 +25,7 @@ def create_app():
         openapi_url=f"{settings.API_V1_STR}/openapi.json",  # 将 API 文档放在自定义路径以避免默认路径被暴露
         # generate_unique_id_function=custom_generate_unique_id,  # TODO 通过自定义逻辑增强路由唯一性或实现更符合业务需求的 ID。
         # docs_url=None,  # 关闭默认的文档路由 TODO 有方法关闭部分接口
+        # redoc_url=None,
         lifespan=lifespan
     )
     return app
